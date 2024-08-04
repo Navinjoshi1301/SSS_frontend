249b489b2fb528091addb97c1438f7974203b3b1
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-08-04 23:35:55 +0530

    Implement transition on button

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -4,7 +4,6 @@ const ServiceCard = ({  title, description, logoUrl  }) => {
         <div className="bg-white shadow-lg rounded-3xl p-6 m-4 flex flex-col items-center border-2 border-gray-300">
             <div className='my-5'>
             {logoUrl && <img src={logoUrl} alt={title} className="w-16 h-16 mb-4" />} {/* Display the logo */}
-                <logoUrl className="text-6xl mb-4 text-blue-500" />
             </div>
             <h3 className="text-xl font-bold mb-2 text-center">{title}</h3>
             <p className="text-gray-600 text-center pb-10">{description}</p>
